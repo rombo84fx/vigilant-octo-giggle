3a047c854be4dea6d832ae682c03cf3229539d44
rombo84fx <gmart_nez@yahoo.com.mx>
2015-12-13 13:50:09 -0600

    GarbageCollectionDemo destructor

---
+++ ModuleSix/GarbageCollectionDemo/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionDemo
+{
+    class Calculator
+    {
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+        }
+
+        public int Divide( int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
---
+++ ModuleSix/GarbageCollectionDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("{0} / {1} = {2}", 120, 15, calculator.Divide(120, 15));
+            Console.WriteLine("Program finishing");
+        }
+    }
+}
